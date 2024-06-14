cd47a1ecda04e95d3f14f5ec1d472ee17acd200d
CyberL1 <mateusz.hf18@gmail.com>
2024-06-14 19:37:16 +0200

    Upload files

---
+++ index.js
@@ -0,0 +1,56 @@
+import { AttachmentBuilder, Client } from "discord.js";
+import "dotenv/config";
+import { execSync } from "child_process";
+import {
+  createReadStream,
+  createWriteStream,
+  readFileSync,
+  rmSync,
+  writeFileSync,
+} from "fs";
+
+const client = new Client({
+  intents: ["Guilds", "GuildMessages", "MessageContent"],
+});
+
+client.on("ready", () => {
+  console.log("Bot ready");
+});
+
+client.on("messageCreate", async (message) => {
+  if (!message.author.bot) {
+    if (message.attachments.size && message.attachments.first()) {
+      const attachment = message.attachments.first();
+      const extension = attachment.name.split(".").pop();
+
+      if (extension === "pt3") {
+        const reply = await message.reply("Converting to wav");
+
+        // Convert to .wav
+        const file = await fetch(attachment.url);
+        writeFileSync(attachment.name, Buffer.from(await file.arrayBuffer()));
+
+        execSync(
+          `./zxtune123 --wav filename=${attachment.name}.wav ${attachment.name}`,
+        );
+
+        const wavFile = createReadStream(`./${attachment.name}.wav`);
+
+        reply.edit({
+          content: "done",
+          files: [
+            new AttachmentBuilder()
+              .setName(`${attachment.name}.wav`)
+              .setFile(wavFile),
+          ],
+        });
+
+        // Cleanup
+        rmSync(attachment.name);
+        // rmSync(`${attachment.name}.wav`);
+      }
+    }
+  }
+});
+
+client.login(process.env.BOT_TOKEN);
